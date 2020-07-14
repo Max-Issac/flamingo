4a04a73e4fb01a9454d444afed0bb0480381313c
Issac <tlonly008@gmail.com>
2020-07-14 00:28:25 -0700

    Springboot project initial commit

---
+++ src/test/java/com/livinghood/flamingo/FlamingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livinghood.flamingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlamingoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
